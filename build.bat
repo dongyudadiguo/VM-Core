212aa5ecb04c8d76a80266538c6738b36aa0699c
dongyudadiguo <3662728251@qq.com>
2025-02-09 21:59:25 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,4 @@
-@echo off
-choice /m "add icon?"
-if %errorlevel%==1 (
-	echo A ICON "icon.ico">ico.rc
-	windres ico.rc ico.o
-	gcc *.c ico.o -o tinyvm.exe
-	del ico.rc ico.o
-)
-if %errorlevel%==2 (
-	gcc *.c -o tinyvm.exe
-)
-tinyvm.exe
-pause>nul
+echo A ICON "icon.ico">ico.rc
+windres ico.rc ico.o
+gcc *.c ico.o -o vm.exe
+del ico.rc ico.o
